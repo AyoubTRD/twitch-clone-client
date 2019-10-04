d36af833dbd3f3dc6c298b596fb9718270730d4a
JrTRD <ayoubtrdone@gmail.com>
2019-10-04 19:44:12 +0100

    crud actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,12 @@
-import { AUTHENTICATE, CREATE_STREAM } from "./types";
-import streams from "../api/streams";
+import {
+  AUTHENTICATE,
+  CREATE_STREAM,
+  GET_STREAMS,
+  GET_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM
+} from "./types";
+import streamApi from "../api/streams";
 
 export const authenticate = (isSignedIn, info) => {
   info = info.getBasicProfile();
@@ -19,9 +26,39 @@ export const authenticate = (isSignedIn, info) => {
 };
 
 export const createStream = streamInfo => async dispatch => {
-  await streams.post("/streams", streamInfo);
+  await streamApi.post("/streams", streamInfo);
   return dispatch({
     type: CREATE_STREAM,
     payload: streamInfo
+  });
+};
+
+export const getStreams = () => async dispatch => {
+  const res = await streamApi.get("/streams");
+  dispatch({
+    type: GET_STREAMS,
+    payload: res.data
+  });
+};
+
+export const getStream = id => async dispatch => {
+  const res = await streamApi.get(`/streams/${id}`);
+  dispatch({
+    type: GET_STREAM,
+    payload: res.data
+  });
+};
+
+export const deleteStream = id => async dispatch => {
+  await streamApi.delete(`/streams/${id}`);
+  dispatch({
+    type: DELETE_STREAM
+  });
+};
+
+export const editStream = newStream => async dispatch => {
+  await streamApi.put(`/streams`, newStream);
+  dispatch({
+    type: EDIT_STREAM
   });
 };
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,2 +1,6 @@
 export const AUTHENTICATE = "AUTHENTICATE";
 export const CREATE_STREAM = "CREATE_STREAM";
+export const GET_STREAMS = "GET_STREAMS";
+export const GET_STREAM = "GET_STREAM";
+export const DELETE_STREAM = "DELETE_STREAM";
+export const EDIT_STREAM = "EDIT_STREAM";
