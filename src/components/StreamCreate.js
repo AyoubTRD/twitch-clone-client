04320d038a6d533485b92ad06b80cf2704b9bf2d
JrTRD <ayoubtrdone@gmail.com>
2019-10-04 19:36:41 +0100

    redux-form Field fix

--- src/components/StreamCreate.js
+++ src/components/StreamCreate.js
@@ -1,61 +1,73 @@
 import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
 
-class StreamCreate extends Component {
-  renderInput({ name, id, placeholder, type, label, classes }) {
-    return function({ input }) {
-      const lastClasses = classes ? `form__input ${classes}` : "form__input";
+const renderInput = ({
+  info: { name, id, placeholder, type, label, classes },
+  input
+}) => {
+  
+  const lastClasses = classes ? `form__input ${classes}` : "form__input";
 
-      return (
-        <span>
-          {label ? (
-            <label for={id} className="form__label">
-              {label}
-            </label>
-          ) : null}
-          <input
-            name={name}
-            placeholder={placeholder}
-            type={type}
-            id={id}
-            className={lastClasses}
-            {...input}
-          />
-        </span>
-      );
-    };
-  }
+  return (
+    <span>
+      {label ? (
+        <label htmlFor={id} className="form__label">
+          {label}
+        </label>
+      ) : null}
+      <input
+        name={name}
+        placeholder={placeholder}
+        type={type}
+        id={id}
+        className={lastClasses}
+        {...input}
+      />
+    </span>
+  );
+};
+
+class StreamCreate extends Component {
+  onSubmit = ({ title, description }) => {
+    console.log(title, description);
+  };
 
   render() {
+    const { handleSubmit } = this.props;
     return (
       <div className="container">
-        <form className="form">
+        <h1>Create a stream</h1>
+        <form
+          className="create-form form"
+          onSubmit={handleSubmit(this.onSubmit)}
+        >
           <div className="form-box">
             <Field
               name="title"
-              component={this.renderInput({
+              component={renderInput}
+              info={{
                 name: "title",
                 placeholder: "title",
                 type: "text",
-                id: "title"
-              })}
+                id: "title",
+              }}
             />
           </div>
           <div className="form-box">
             <Field
               name="description"
-              component={this.renderInput({
+              component={renderInput}
+              info={{
                 name: "description",
                 placeholder: "description",
                 type: "text",
                 id: "description"
-              })}
+              }}
             />
-            <br />
-            <button type="submit" className="btn btn-main">
-              Create
-            </button>
           </div>
+          <button type="submit" className="btn btn-main form__submit">
+            Create
+          </button>
         </form>
       </div>
     );
